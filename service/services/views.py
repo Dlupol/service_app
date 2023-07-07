95766712eb91d2e3bc0038d025ef707ed4c88e15
Dmitry Lupol <dmitry.lupol@gmail.com>
2023-07-07 17:03:47 +0300

    day 5

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.cache import cache
 from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import *
@@ -8,15 +10,20 @@ from clients.models import Clients
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
-        Prefetch('client', queryset=Clients.objects.all().select_related('user').only('company_name', 'user__email'))
-    ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
+        Prefetch('client', queryset=Clients.objects.all().select_related('user').only('company_name', 'user__email')))
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 60)
 
         response_data = {'result': response.data}
-        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response_data['total_amount'] = total_price
         response.data = response_data
         return response
